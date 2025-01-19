ede43af092353be9f78dd62ef0d4a095cec4b3c6
jeonni <>
2025-01-19 18:11:25 +0900

    RECORD: [보충] 캡슐화 추가 설명

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import { CatsService } from './cats/cats.service';
 
 // ** Nest JS 에서는 의존성을 명료하게 함으로써 공급자-소비자 역할을 확실히 정의하고 있음.
 @Module({
-  imports: [CatsModule, UsersModule], // 모듈을 연결(가져오는)하는 것
+  // ** 모듈을 연결(가져오는)하는 것
+  // - 모듈에서 만든 공급자들은 여기서 퍼블릭으로 만들어 사용할 수 있도록 하는 것이 좋은 구조 (각 모듈의 exports 영역에 추가)
+  imports: [CatsModule, UsersModule],
   controllers: [AppController],
+  // - 자체적으로 만든 공급자는 해당 영역에 넣어주는 것이 좋음. (중요)
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -5,8 +5,10 @@ import { UsersModule } from 'src/users/users.module';
 
 // module의 provider
 @Module({
-    controllers: [CatsController],
-    providers: [CatsService],
-    exports: [CatsService], // *** 해당 module 에서 export 해줘야만 app.controller에서 cats.service를 사용할 수 있다.
+  controllers: [CatsController],
+  providers: [CatsService],
+  // *** 해당 module 에서 export 해줘야만 app.controller에서 cats.service를 사용할 수 있다.
+  // - provider로서 CatsService를 만들어서 원래는 캡슐화되어있는 상태이기에 다른 모듈에서는 접근 불가능한 상태이나 exports 를 통해 다른 모듈에서도 접근 가능하도록 설정 
+  exports: [CatsService],
 })
 export class CatsModule {}
